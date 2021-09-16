f9dc7f394b937cff3cdd10b2590e44810371e202
ga-felix <gafelix435@gmail.com>
2021-09-16 18:41:39 -0300

    Create classification model

---
+++ classifier/k-nearest/model.py
@@ -0,0 +1,21 @@
+"""
+    This files creates a classification model based on K-nearest
+    neighbors.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+""" Reading the dataset """
+df = pd.read_csv('teleCust1000t')
+print(df.describe())
+print(df['custcat'].value_counts())
+
+""" In order to use Scikit-learn library, a numpy array is needed """
+x = df [['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']].values
+x[0:5]
+y = df['custcat'].values
+y[0:5]
